ce32c50657d8a00f89c5bdcdf69201cafa79aaf1
Patrick Reich <patrick@nfits.de>
2024-06-24 11:59:21 +0200

    add daniel

--- nix/modules/config/root-user.nix
+++ nix/modules/config/root-user.nix
@@ -3,6 +3,7 @@
 {
   users.users.root = {
     openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFZPYF6pCjSFVkxJOMw1DuiFaXoDfa2lynxVu+/u5Qu2 daniel@nfits"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIALcss3gZalQ+NZej76HdmT01yfgnZUUg2ArydmL+ZQ0 patrick@nfits"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH7W3NIGeEGRHu63+dP7s6M5/s0uHODI4QV2Y1yOzDEq lukas2511@redrocket"
     ];
