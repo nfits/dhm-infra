8bfabad35ef7008ce6b4afdc0a6a425cb34a0215
localo <git@localo.ooo>
2024-06-25 21:26:33 +0200

    pass flake inputs to system flake registry

--- nix/modules/nix/default.nix
+++ nix/modules/nix/default.nix
@@ -1,15 +1,22 @@
-{ lib, pkgs, ... }:
+{ lib, pkgs, inputs, ... }:
 
 with builtins;
 with lib;
 {
-  imports = mapAttrsToList (n: _: ./${n}) (
-    filterAttrs (n: t: n != "default.nix" && t == "regular") (readDir ./.)
-  );
+  imports = mapAttrsToList
+    (n: _: ./${n})
+    (filterAttrs (n: t: n != "default.nix" && t == "regular") (readDir ./.));
 
   nix = mkDefault {
     package = pkgs.nixFlakes;
-    extraOptions = "experimental-features = nix-command flakes";
+    settings.experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+
+    registry = (lib.mapAttrs (_: flake: { inherit flake; })) (
+      (lib.filterAttrs (_: lib.isType "flake")) inputs
+    );
 
     gc = {
       automatic = true;
