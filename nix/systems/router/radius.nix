a34828ea664c83cdad2c1b18ee49f1ab12b8611e
Patrick Reich <patrick@nfits.de>
2024-07-03 02:02:39 +0200

    radius: configure outer tunnel

--- nix/systems/router/radius.nix
+++ nix/systems/router/radius.nix
@@ -11,20 +11,22 @@ let
   vlanCfg = config.dhm.networking.vlans;
   pkg = pkgs.freeradius;
 
+  certDir = config.security.acme.certs."dhm-ctf.de".directory;
+
   port = 1812;
 
   usersFile = pkgs.writeText "users" ''
     ${concatStringsSep "\n" (
       map (user: ''
         ${user}
-                Tunnel-Private-Group-Id = ${toString vlanCfg.${user}.vlanId},
+                Tunnel-Private-Group-Id := ${toString vlanCfg.${user}.vlanId},
                 Fall-Through            = Yes
       '') ([ "organisers" ] ++ (map (id: "team-${toString id}") (range 1 14)))
     )}
 
     DEFAULT
-            Tunnel-Type        = VLAN,
-            Tunnel-Medium-Type = IEEE-802
+            Tunnel-Type        := VLAN,
+            Tunnel-Medium-Type := IEEE-802
   '';
 
   configText = ''
@@ -75,7 +77,8 @@ let
         filename = ${usersFile}
       }
 
-      eap inner-eap {
+      eap {
+        max_sessions = ''${max_requests}
         default_eap_type = mschapv2
 
         gtc {
@@ -83,10 +86,98 @@ let
         }
 
         mschapv2 { }
+
+        tls-config tls-common {
+          private_key_file = "${certDir}/key.pem"
+          certificate_file = "${certDir}/cert.pem"
+
+          cache {
+            enable = no
+            persist_dir = "/var/lib/radius"
+
+            store {
+              Tunnel-Private-Group-Id
+            }
+          }
+
+          cipher_list = "DEFAULT"
+
+          tls_min_version = "1.2"
+          tls_max_version = "1.3"
+        }
+
+        tls {
+          tls = tls-common
+        }
+
+        ttls {
+          tls = tls-common
+          default_eap_type = pap
+          virtual_server = "inner-tunnel"
+
+          use_tunneled_reply = yes
+        }
+
+        peap {
+          tls = tls-common
+          default_eap_type = mschapv2
+          virtual_server = "inner-tunnel"
+
+          use_tunneled_reply = yes
+        }
       }
     }
 
-    # ... log, client, thread pool, module, policy, and server config...
+    server default {
+      listen {
+        type = auth
+        ipaddr = *
+        port = ${toString port}
+      }
+
+      limits {
+        max_connections = 16
+        lifetime = 0
+        idle_timeout = 30
+      }
+
+      authorize {
+        filter_username
+
+        chap
+        mschap
+
+        eap {
+          ok = return
+          updated = return
+        }
+
+        dhm_passwd
+        files
+
+        expiration
+        logintime
+
+        pap
+      }
+
+      authenticate {
+        Auth-Type PAP {
+          pap
+        }
+
+        Auth-Type CHAP {
+          chap
+        }
+
+        Auth-Type MS-CHAP {
+          mschap
+        }
+
+        eap
+      }
+    }
+
     server inner-tunnel {
       listen {
         type = auth
@@ -100,7 +191,7 @@ let
         chap
         mschap
 
-        inner-eap {
+        eap {
           ok = return
         }
 
@@ -126,15 +217,26 @@ let
           mschap
         }
 
-        inner-eap
+        eap
       }
 
       post-auth {
+        update reply {
+          User-Name !* ANY
+          Message-Authenticator !* ANY
+          EAP-Message !* ANY
+          Proxy-State !* ANY
+          MS-MPPE-Encryption-Types !* ANY
+          MS-MPPE-Encryption-Policy !* ANY
+          MS-MPPE-Send-Key !* ANY
+          MS-MPPE-Recv-Key !* ANY
+        }
+
         Post-Auth-Type REJECT {
           attr_filter.access_reject
 
-          update outer.session-state {
-            &Module-Failure-Message := &request:Module-Failure-Message
+          update reply {
+            Module-Failure-Message := &request:Module-Failure-Message
           }
         }
       }
@@ -169,6 +271,25 @@ let
   '';
 in
 {
+  environment.systemPackages = [
+    (pkgs.wpa_supplicant.overrideAttrs (old: {
+      name = "eapol_test-${old.version}";
+
+      buildPhase = ''
+        runHook preBuild
+        echo CONFIG_EAPOL_TEST=y >> .config
+        make eapol_test
+        runHook postBuild
+      '';
+
+      installPhase = ''
+        install -D eapol_test $out/bin/eapol_test
+      '';
+
+      NIX_CFLAGS_COMPILE = [ "-Wno-error" ];
+    }))
+  ];
+
   sops.secrets = {
     passwords = {
       sopsFile = "${self}/secrets/credentials.sops.yaml";
@@ -176,11 +297,26 @@ in
       mode = "0400";
       owner = "radius";
     };
+
+    cloudflareCredentials.sopsFile = "${self}/secrets/misc.sops.yaml";
   };
 
   users = {
     users.radius.group = "radius";
     groups.radius = { };
+  };
+
+  security.acme = {
+    acceptTerms = true;
+    defaults.email = "infrastructure@nfits.de";
+
+    certs."dhm-ctf.de" = {
+      dnsProvider = "cloudflare";
+      dnsResolver = "1.1.1.1";
+      environmentFile = config.sops.secrets.cloudflareCredentials.path;
+
+      group = "radius";
+    };
   };
 
   services.freeradius = {
@@ -189,4 +325,6 @@ in
 
     inherit configDir;
   };
+
+  systemd.services.freeradius.serviceConfig.StateDirectory = [ "radius" ];
 }
